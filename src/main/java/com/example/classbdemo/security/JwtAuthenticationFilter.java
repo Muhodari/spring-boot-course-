ddfdc7f1989fe5d9d44f4285a0a8532e7e573735
Muhodari <sagemuho@gmail.com>
2021-06-09 21:58:39 +0200

    added new Hbnt

--- src/main/java/com/example/classbdemo/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/classbdemo/security/JwtAuthenticationFilter.java
@@ -56,3 +56,4 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return null;
     }
 }
+
--- src/main/java/com/example/classbdemo/security/JwtTokenProvider.java
+++ src/main/java/com/example/classbdemo/security/JwtTokenProvider.java
@@ -78,6 +78,4 @@ public class JwtTokenProvider {
         }
         return false;
     }
-
-
 }
